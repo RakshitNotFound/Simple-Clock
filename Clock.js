b9a0fd63db0f04cdc9dadeb9305b68035734a28d
RakshitNotFound <99888381+RakshitNotFound@users.noreply.github.com>
2022-10-08 22:15:05 +0530

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -3,3 +3,4 @@ function updateClock(){
     let currentTime = new Date()
     let currentHour = currentTime.getHours();
     let currentMinutes = currentTime.getMinutes();
+    let currentSeconds = currentTime.getSeconds();
