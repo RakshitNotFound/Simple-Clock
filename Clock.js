f9ccebf6cec24872dabc746b9ba09c1eb8629d57
RakshitNotFound <99888381+RakshitNotFound@users.noreply.github.com>
2022-10-08 22:17:52 +0530

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -8,3 +8,4 @@ function updateClock(){
     currentHour = (currentHour < 10  ? "0" : "") + currentHour;
     currentSeconds = (currentSeconds < 10  ? "0" : "") + currentSeconds;
     let TimeOfDay = (currentHour < 12) ? "AM" : "PM";
+    let currentTimeStr = currentHour + ":" + currentMinutes +":" + currentSeconds + " " + TimeOfDay;
