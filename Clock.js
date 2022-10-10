82e503d82ed8a5b9a867cffbe66b4da9028dcfe3
RakshitNotFound <99888381+RakshitNotFound@users.noreply.github.com>
2022-10-10 20:50:25 +0530

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -1,14 +1,31 @@
 console.log("This is Clock.js")
+
 function updateClock(){
+//Get the current date
     let currentTime = new Date()
+
+    //Extract hour,minute and seconds from the date
     let currentHour = currentTime.getHours();
     let currentMinutes = currentTime.getMinutes();
     let currentSeconds = currentTime.getSeconds();
+
+
+    //pad 0 if minutes and seconds are less than 10(that means in double digit)
     currentMinutes = (currentMinutes < 10  ? "0" : "") + currentMinutes;
     currentHour = (currentHour < 10  ? "0" : "") + currentHour;
     currentSeconds = (currentSeconds < 10  ? "0" : "") + currentSeconds;
+
+    //Railway clock to AM AND PM.
     let TimeOfDay = (currentHour < 12) ? "AM" : "PM";
-    let currentTimeStr = currentHour + ":" + currentMinutes +":" + currentSeconds + " " + TimeOfDay;
+
+currentHour = (currentHour>12) ? currentHour - 12 : currentHour;
+currentHour = (currentHour==0) ? 12 : currentHour;
+
+
+
+
+    let currentTimeStr = currentHour + ":" + currentMinutes +":" + currentSeconds + " " + TimeOfDay;  
+
     document.getElementById("Clock").innerHTML = currentTimeStr;
 
 
